59e3c843603d4b49ee6bfd8bdfc014200d4e162c
Yuri Norwood <76186754+yuri-norwood@users.noreply.github.com>
2021-03-19 12:19:00 +1300

    Update tabs

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "github.com/yuri-norwood/bfk"
+	"github.com/yuri-norwood/bfk"
 )
 
 const HelloWorld = "+[>>>->-[>->----<<<]>>]>.---.>+..+++.>>.<.>>---.<<<.+++.------.<-.>>+."
 
 func main() {
-    program := bfk.Parse(HelloWorld)
-    program.Execute(os.Stdout) // Prints "hello, world!"
+	program := bfk.Parse(HelloWorld)
+	program.Execute(os.Stdout) // Prints "hello, world!"
 }
